4ee5f526c5022ec56758ae8eced947d3c51c5f72
Gabriel Martins <gabriel.santos@egs.com.br>
2020-05-28 17:20:23 -0300

    cria state para clientes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
-import  Tabela from './Tabela';
+import Tabela from './Tabela';
 
-function App() {
 
-  const clientes = [
-    {
-      nome: 'Juliana',
-      profissao: 'Dentista'
-    },
-    {
-      nome: 'Kleber',
-      profissao: 'Caminhoneiro'
-    },
-    {
-      nome: 'César',
-      profissao: 'Jornalista'
-    },
-    {
-      nome: 'Matilde',
-      profissao: 'Psicóloga'
-    }
-  ];
+class App extends Component {
+  
+  state = {
+    clientes: [
+      {
+        nome: 'Juliana',
+        profissao: 'Dentista'
+      },
+      {
+        nome: 'Kleber',
+        profissao: 'Caminhoneiro'
+      },
+      {
+        nome: 'César',
+        profissao: 'Jornalista'
+      },
+      {
+        nome: 'Matilde',
+        profissao: 'Psicóloga'
+      }
+    ]
+  };
 
-  return (
-    <div className="App">
-      <Tabela clientes = { clientes } />
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <Tabela clientes={this.state.clientes} />
+      </div>
+    );
+  }
 }
 
 export default App;
