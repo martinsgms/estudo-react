93e6639e26eb412d0caeb0f87852edd3f8d496b5
Gabriel Martins <gabriel.santos@egs.com.br>
2020-06-07 23:52:56 -0300

    Updates

---
+++ precache-manifest.35f20a6375832da55a3685eee3b91134.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72fdae49fcf08fc7f3766692d813d6d4",
+    "url": "/estudo-react/index.html"
+  },
+  {
+    "revision": "55647dbe38474cf6adea",
+    "url": "/estudo-react/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "acd5f4bd29ab71f28526",
+    "url": "/estudo-react/static/css/main.ccbf82b9.chunk.css"
+  },
+  {
+    "revision": "55647dbe38474cf6adea",
+    "url": "/estudo-react/static/js/2.4d5b61b5.chunk.js"
+  },
+  {
+    "revision": "33cf65b503bb75abe281153a2d35bc26",
+    "url": "/estudo-react/static/js/2.4d5b61b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acd5f4bd29ab71f28526",
+    "url": "/estudo-react/static/js/main.55a42381.chunk.js"
+  },
+  {
+    "revision": "dd2ad50ed2595d02ebb5",
+    "url": "/estudo-react/static/js/runtime-main.551ca6c9.js"
+  }
+]);
